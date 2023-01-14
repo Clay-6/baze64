416f3610fe313cee2d3a5fc5ac2685f7a030b584
Clay <claytonwambu@gmail.com>
2023-01-14 20:56:39 +0000

    Add lib docs

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Baze64
 
-A Rust crate to encode & decode Base64
+A Rust library & binary crate for encoding & decoding
+base64
+
+## Usage
+
+Run `baze64 encode <STRING>` to encode a string or `baze64 encode -f <FILE>` to
+encode a file. Decode a base64 string by running
+
+```shell
+baze64 decode <STRING>
+```
+
+where `<STRING>` is a base64 encoded string, adding `-o <FILE>` to output to `<FILE>`
+
+## Installation
+
+### Through Cargo
+
+Run the command
+
+```shell
+cargo install baze64
+```
+
+### Precompiled Binary
+
+Install the binary for your system from the [latest GitHub release](https://github.com/Clay-6/baze64/releases/latest)
+and extract the binary to a location on your `PATH`
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+//! Encode & decode base64 to & from arbitrary byte sequences
+//!
+//! All encoding & decoding is done via the [`Base64String`]
+//! struct, using an alphabet implementing the [`Alphabet`](alphabet::Alphabet)
+//! trait
+//!
+//! ## Examples
+//!
+//! Encode & decode a string:
+//! ```
+//! # use baze64::{Base64String, alphabet::Standard};
+//! let text = "Some text".to_string();
+//! let encoded: Base64String<Standard> = Base64String::encode(text.as_bytes());
+//! let decoded = encoded.decode();
+//!
+//! assert_eq!(text, String::from_utf8(decoded)?);
+//! # Ok::<(), std::string::FromUtf8Error>(())
+//! ```
+//!
+//! Encode & decode a file:
+//! ```no_run
+//! # use std::{fs::File, io::Read};
+//! # use baze64::{Base64String, alphabet::Standard};
+//! let mut file = File::open("path/to/file.ext")?;
+//! let mut buffer = Vec::new();
+//! file.read_to_end(&mut buffer)?;
+//! let encoded: Base64String<Standard> = Base64String::encode(&buffer);
+//! let bytes = encoded.decode();
+//!
+//! assert_eq!(buffer, bytes);
+//! # Ok::<(), std::io::Error>(())
+//! ```
+//!
+
 pub mod alphabet;
 mod base64string;
 
