c836436ade2d70416ad6c4ba9878fc3b5d813f92
Clay <claytonwambu@gmail.com>
2023-02-12 15:23:01 +0000

    Scaffold GUI

--- crates/baze64-gui/src/main.rs
+++ crates/baze64-gui/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use eframe::{egui, epaint::Vec2, NativeOptions};
+
+fn main() -> eframe::Result<()> {
+    let options = NativeOptions {
+        initial_window_size: Some(Vec2::new(320.0, 230.0)),
+        ..Default::default()
+    };
+
+    eframe::run_native("Baze64", options, Box::new(|_cc| Box::<App>::default()))
+}
+
+#[derive(Debug, Default)]
+struct App {}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| ui.heading("Baze64"));
+    }
 }
