2cf573f22aaf821a94321fab3e797de39219c617
Clay <clay@clay66.dev>
2023-09-23 00:19:09 +0100

    Set plaintext & base64 textboxes on enter key pressed

--- crates/baze64-gui/src/main.rs
+++ crates/baze64-gui/src/main.rs
@@ -12,20 +12,24 @@ fn main() {
     let mw_weak = main_window.as_weak();
     main_window.on_encode_plaintext(move |text| {
         info!("on_decode_plaintext");
-        let encoded = Base64String::<Standard>::encode(text.as_bytes()).unwrap();
-        debug!(?text, ?encoded, "encoded plaintext");
+        let encoded = Base64String::<Standard>::encode(text.as_bytes())
+            .unwrap()
+            .to_string();
+        info!(?text, ?encoded, "encoded plaintext");
         let mw = mw_weak.unwrap();
-        mw.set_base64(encoded.to_string().into());
-        debug!("set base64 text field");
+        mw.invoke_set_base64(encoded.into());
+        info!("set base64 text field");
     });
     let mw_weak = main_window.as_weak();
     main_window.on_decode_base64(move |base64| {
         info!("on_decode_base64");
-        let decoded = Base64String::<Standard>::from_encoded(&base64);
-        debug!(?base64, ?decoded, "decoded base64");
+        let decoded = Base64String::<Standard>::from_encoded(&base64)
+            .decode_to_string()
+            .unwrap();
+        info!(?base64, ?decoded, "decoded base64");
         let mw = mw_weak.unwrap();
-        mw.set_plaintext(decoded.decode_to_string().unwrap().into());
-        debug!("set plaintext text field");
+        mw.invoke_set_plaintext(decoded.into());
+        info!("set plaintext text field");
     });
 
     main_window.run().unwrap();
