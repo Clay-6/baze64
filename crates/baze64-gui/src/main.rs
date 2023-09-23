3e64b3cab8fa200f0b771697fba2fe820e1e7624
Clay <clay@clay66.dev>
2023-09-23 15:21:52 +0100

    Add basic error handling

--- crates/baze64-gui/src/main.rs
+++ crates/baze64-gui/src/main.rs
@@ -1,10 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use baze64::{
-    alphabet::{Standard, UrlSafe},
+    alphabet::{Alphabet, Standard, UrlSafe},
     Base64String,
 };
-use tracing::{debug, info};
+use tracing::{debug, error, info};
 
 slint::include_modules!();
 
@@ -18,14 +18,17 @@ fn main() {
     main_window.on_encode_plaintext(move |text| {
         let mw = mw_weak.unwrap();
         let encoded = match mw.invoke_get_current_alphabet().as_str() {
-            "standard" => Base64String::<Standard>::encode(text.trim().as_bytes())
-                .unwrap()
-                .to_string(),
-            "urlsafe" => Base64String::<UrlSafe>::encode(text.trim().as_bytes())
-                .unwrap()
-                .to_string(),
+            "standard" => Base64String::encode_with(text.trim().as_bytes(), Alpha::Standard),
+            "urlsafe" => Base64String::encode_with(text.trim().as_bytes(), Alpha::UrlSafe),
             other => panic!("How is the alphabet {other}"),
         };
+        let encoded = encoded.map_or_else(
+            |e| {
+                error!(?e);
+                "[Error encoding text]".to_string()
+            },
+            |s| s.to_string(),
+        );
         info!(?text, ?encoded, "encoded plaintext");
         mw.invoke_set_base64(encoded.into());
         info!("set base64 text field");
@@ -35,18 +38,50 @@ fn main() {
     main_window.on_decode_base64(move |base64| {
         let mw = mw_weak.unwrap();
         let decoded = match mw.invoke_get_current_alphabet().as_str() {
-            "standard" => Base64String::<Standard>::from_encoded(base64.trim())
-                .decode_to_string()
-                .unwrap(),
-            "urlsafe" => Base64String::<UrlSafe>::from_encoded(base64.trim())
-                .decode_to_string()
-                .unwrap(),
+            "standard" => Base64String::from_encoded_with(&base64, Alpha::Standard),
+            "urlsafe" => Base64String::from_encoded_with(&base64, Alpha::UrlSafe),
             other => panic!("How is the alphabet {other}"),
-        };
+        }
+        .decode_to_string()
+        .map_or_else(
+            |e| {
+                error!(?e);
+                "[Error decoding]".to_string()
+            },
+            |s| s.to_string(),
+        );
         info!(?base64, ?decoded, "decoded base64");
         mw.invoke_set_plaintext(decoded.into());
         info!("set plaintext text field");
     });
 
     main_window.run().unwrap();
+}
+
+enum Alpha {
+    Standard,
+    UrlSafe,
+}
+
+impl Alphabet for Alpha {
+    fn padding(&self) -> Option<char> {
+        match self {
+            Alpha::Standard => Standard::new().padding(),
+            Alpha::UrlSafe => UrlSafe::new().padding(),
+        }
+    }
+
+    fn encode_bits(&self, bits: u8) -> Result<char, baze64::B64Error> {
+        match self {
+            Alpha::Standard => Standard::new().encode_bits(bits),
+            Alpha::UrlSafe => UrlSafe::new().encode_bits(bits),
+        }
+    }
+
+    fn decode_char(&self, c: char) -> Result<u8, baze64::B64Error> {
+        match self {
+            Alpha::Standard => Standard::new().decode_char(c),
+            Alpha::UrlSafe => UrlSafe::new().decode_char(c),
+        }
+    }
 }
