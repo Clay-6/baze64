fa2cfe83bd7d3f9e58c6a13edc7b3eb1953edd9e
Clay <clayw@clay66.dev>
2023-10-20 20:35:51 +0100

    Add fuzz target to ensure parity with `base64` crate

---
+++ crates/baze64/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -0,0 +1,13 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+use base64::{engine::general_purpose, Engine as _};
+use baze64::{alphabet::Standard, Base64String};
+
+fuzz_target!(|data: &[u8]| {
+    let baze = Base64String::<Standard>::encode(data).unwrap().to_string();
+    let reference = general_purpose::STANDARD.encode(data);
+
+    assert_eq!(baze, reference);
+});
