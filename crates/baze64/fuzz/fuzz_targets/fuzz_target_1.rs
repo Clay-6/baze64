6a701a7ec6b6cdd773a839c691fe32f3868cbd9a
Clay <clayw@clay66.dev>
2023-10-27 23:16:27 +0100

    Update fuzz target

--- crates/baze64/fuzz/fuzz_targets/fuzz_target_1.rs
+++ crates/baze64/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -6,7 +6,7 @@ use base64::{engine::general_purpose, Engine as _};
 use baze64::{alphabet::Standard, Base64String};
 
 fuzz_target!(|data: &[u8]| {
-    let baze = Base64String::<Standard>::encode(data).unwrap().to_string();
+    let baze = Base64String::<Standard>::encode(data).to_string();
     let reference = general_purpose::STANDARD.encode(data);
 
     assert_eq!(baze, reference);
