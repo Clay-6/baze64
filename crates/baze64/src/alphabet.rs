52ac20d2e37a2b018355b5ef82ba87e49d07dd9a
Clay <clayw@clay66.dev>
2023-10-27 22:53:23 +0100

    Make `Standard` & `UrlSafe` constructors `const fn`

--- crates/baze64/src/alphabet.rs
+++ crates/baze64/src/alphabet.rs
@@ -23,14 +23,7 @@ pub struct Standard {
 }
 
 impl Standard {
-    pub fn new() -> Self {
-        Self::default()
-    }
-}
-
-impl Default for Standard {
-    /// Get a new [`Standard`] alphabet, using its [`Default`] impl
-    fn default() -> Self {
+    pub const fn new() -> Self {
         Self {
             encode_map: [
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -42,15 +35,16 @@ impl Default for Standard {
     }
 }
 
-impl UrlSafe {
-    /// Get a new [`UrlSafe`] Alphabet, using its [`Default`] impl
-    pub fn new() -> Self {
-        Self::default()
+impl Default for Standard {
+    /// Get a new [`Standard`] alphabet, using its [`Default`] impl
+    fn default() -> Self {
+        Self::new()
     }
 }
 
-impl Default for UrlSafe {
-    fn default() -> Self {
+impl UrlSafe {
+    /// Get a new [`UrlSafe`] Alphabet, using its [`Default`] impl
+    pub const fn new() -> Self {
         Self {
             encode_map: [
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -59,6 +53,12 @@ impl Default for UrlSafe {
                 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_',
             ],
         }
+    }
+}
+
+impl Default for UrlSafe {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
